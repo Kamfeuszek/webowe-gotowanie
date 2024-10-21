6f79db5cc3f74a95f1f396bffafa3484ba02dbad
Kamfeuszek <florczakk01@gmail.com>
2024-10-21 21:45:05 +0200

    js connect json + html

---
+++ centrala/assets/js/main.js
@@ -0,0 +1,15 @@
+function createMenu() {
+    $.getJSON('menu.json', function(json) {
+        var menu = json.menu;
+        let sidebar = document.getElementById("sidebarbody"); 
+        var content = "";
+
+        menu.forEach(function(item)  {
+            if(item.active == 1) {
+                content += "<a href='" + item.url + "'>" + item.name + "</a><br><br>";
+            }
+        });
+        sidebar.innerHTML = content;
+    })
+}
+createMenu();
