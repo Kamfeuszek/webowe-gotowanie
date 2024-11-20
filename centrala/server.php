dd95b8d89bc59dc1116b7b0596e886f72ab54857
Kamfeuszek <florczakk01@gmail.com>
2024-11-20 15:13:33 +0100

    lekcja 13

---
+++ centrala/server.php
@@ -0,0 +1,21 @@
+<?php
+
+if(isset($_POST))
+{
+	$file = $_POST['jsonFile'];
+	$json = file_get_contents($file . '.json');
+
+	if(isset($_POST['id']))
+	{
+		$id = $_POST['id'];
+		$status = $_POST['status'];
+		$name = $_POST['name'];
+		$obj = json_decode($json);
+		$obj->placement->{$id}->switchStatus = $status;
+		
+		
+		file_put_contents($file . '.json', json_encode($obj));
+		$json = file_get_contents($file . '.json');
+	}
+	echo $json;
+}
